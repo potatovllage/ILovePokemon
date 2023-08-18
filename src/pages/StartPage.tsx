03fad6e42f57074f5b9ed3c7e13b3ce7d63a804d
Yeong joon <bluehome8626@naver.com>
2023-08-18 16:35:01 +0900

    refactoring : start page style change

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -2,14 +2,22 @@ import style from "../styles/pages/start.module.scss";
 import bind from "../styles/cx";
 import ball from "../assets/MonsterBall.png";
 import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const cx = bind(style);
 
 function StartPage() {
+  const navigate = useNavigate();
+
   return (
     <div className={cx(style.Wrapper)}>
       <img height={600} src={logo} alt="logo" />
-      <img className={cx(style.MonsterBall)} src={ball} alt="pokeball" />
+      <img
+        onClick={() => navigate("/main")}
+        className={cx(style.MonsterBall)}
+        src={ball}
+        alt="pokeball"
+      />
     </div>
   );
 }
