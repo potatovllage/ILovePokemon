40e825a236d4e7117c01d7c9667248820113a859
Yeong joon <bluehome8626@naver.com>
2023-08-21 11:31:19 +0900

    refactoirng : modify heaer style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import style from "./style.module.scss";
 import bind from "../../styles/cx";
 import monsterBall from "../../assets/image/MonsterBall.png";
+import globe from "../../assets/image/Globe.png";
 import { useNavigate } from "react-router-dom";
 
 const cx = bind(style);
@@ -13,6 +14,12 @@ function Header() {
       <div className={cx(style.HeaderContainer)}>
         <div onClick={() => naviagate("/")} className={cx(style.LogoContainer)}>
           <img height={50} src={monsterBall} alt="mainlogo" />
+        </div>
+        <div className={cx(style.ChangeLanguageContainer)}>
+          <img height={24} src={globe} alt="globe" />
+          <p className={cx(style.Language)}>English</p>
+          <p>|</p>
+          <p className={cx(style.Language)}>한국어</p>
         </div>
       </div>
     </header>
