0afb8c668aee7cc7b8389100e1292b8ff62be36e
Yeong joon <bluehome8626@naver.com>
2023-08-24 15:47:21 +0900

    chore : react query setting

---
+++ src/config/queryClient.ts
@@ -0,0 +1,12 @@
+import { QueryClient } from "react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000,
+      refetchInterval: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
